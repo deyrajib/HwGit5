7a64f499d2d90e4cf44ed3076c314d2a347de8c8
deyrajib <dey.rajib233@gmail.com>
2022-04-11 20:24:30 -0400

    Hw

---
+++ src/hw/BasicJava.java
@@ -0,0 +1,49 @@
+package hw;
+
+public class BasicJava {
+
+	 // class start point
+		
+
+		
+		// main method need to create
+		
+		public static void main( String args[]  ) {   // main method start point
+			
+			// main method is called the heart of a class
+			// to execute any code we need main method
+			// main method is called driver / executable method
+			
+			
+			// lkjljkes lksdjlsdjfsld sdl jldjlsd this is not code. this is called commenting
+			
+			
+			// To print anything
+			// ; is used to finish a statement or line.
+			System.out.println(       "  Hello "    );
+			
+			System.out.println(       "   World "    );
+			
+			System.out.println(       "   hi "    );
+			
+			System.out.println(       " Raj "    );
+			
+			
+			System.out.print("*********************** We are learning *******************");
+			
+			
+			
+			// double quote is used for String purpose : Any character you typed will print as it is
+			
+			
+		} // main method end point
+		
+        } // class end point
+		
+		// java compiler compile the source code
+		// jvm will execute the class file
+		
+		
+
+	
+
---
+++ src/hw/BlockJava.java
@@ -0,0 +1,25 @@
+package hw;
+
+public class BlockJava {
+	
+
+
+
+		//       { Start  *********** End   }  its called block 
+		
+	   
+			
+		
+		       
+	
+		
+		
+		public static void main (String [] args) {
+		
+		System.out.println("hello");	
+		
+	
+	}
+	
+
+}
